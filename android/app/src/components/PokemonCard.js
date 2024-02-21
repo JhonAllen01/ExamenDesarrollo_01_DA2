5e9e8a63ebc96a08103e72a03de5fa04d38954fb
JV <jhonallen2003@gmail.com>
2024-02-21 15:12:28 -0600

    Creacion de segunda pagina
    Ahora los cards redirigen a la informacion de cada pokemon, la informacion aun no se visualiza

--- android/app/src/components/PokemonCard.js
+++ android/app/src/components/PokemonCard.js
@@ -1,15 +1,21 @@
 // src/components/PokemonCard.js
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 const PokemonCard = ({ pokemon }) => {
+  const navigation = useNavigation();
   const { name, url } = pokemon;
   // Extract the Pokémon ID from the URL
   const pokemonId = url.split('/')[url.split('/').length - 2];
   const imageUrl = `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${name}.png`;
   
+  const onPokemonPress = () => {
+    navigation.navigate('Details', { pokemonName: name });
+  };
+
   return (
-    <TouchableOpacity style={styles.card}>
+    <TouchableOpacity style={styles.card} onPress={onPokemonPress}>
       <Image source={{ uri: imageUrl }} style={styles.cardImage} />
       <Text style={styles.cardText}>{name.charAt(0).toUpperCase() + name.slice(1)}</Text>
     </TouchableOpacity>
